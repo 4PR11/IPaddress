ba40c8c9aa4fc8f82017e1832c8876816fd21303
Mess-sin <Gost1599@gmail.com>
2019-09-24 23:49:05 +0300

    1. Изменение парсера при помощи вложеной и анонимной функций 2. Переработано фунцкия-фабрика(taskStrCon) 3. Улучшен вывод тестов 4. Реалезован  поиск адреса первого узла (adressFirstHost) 5. Реализован поиск адреса последненго узла (adressLastHost) 6. Реализован поиск широковещательного адреса (broadcastAddress) 7. Реализовано нахождение кол-ва узлов в сети (netNodesNum)

--- modules/Gens/Gens.php
+++ modules/Gens/Gens.php
@@ -3,17 +3,24 @@
 const FIRST_IP_BYTE = 10;
 
 //функция-фабрика
-function verGen ($op) {
-	if ($op<0 && $op>2) {
-	 $op = 0;
-	}
+function taskStrCon ($task) {
+	$op = rand(0,3);
+	$result = array('ip' => '', 'mask' => '');
 	if ($op == 0) {
-	//генирация двоичная
+		$result['ip'] = ipBin($task['ip']);
+		$result['mask'] = ipBin($task['mask']);
 	} elseif ($op == 1) {
-	//генирация шеснадцетеричная
-	} else {
-	//генирация десятичная   
+		$result['ip'] = ipHex($task['ip']);
+		$result['mask'] = ipHex($task['mask']);
+	} elseif ($op == 2) {
+		$result['ip'] = ipDec($task['ip']);
+		$result['mask'] ='/'.(string)substr_count(ipBin($task['mask']), '1');
+	} 
+	else {
+		$result['ip'] = ipDec($task['ip']);
+		$result['mask'] = ipDec($task['mask']);
 	}
+	return $result;
 }
 
 function ipGen () {
@@ -51,8 +58,9 @@ function ipDec($ip){
 
 function ipBin($ip){
 	$result = "";
-	for ($i = 0; $i < 4 ; $i++) { 
-		$result .= (string)decbin($ip[$i]) .'.';
+	for ($i = 0; $i < 4 ; $i++) {
+		$buff =(string)decbin($ip[$i]);
+		$result .= substr('00000000', strlen($buff)).$buff.'.';
 	}
 	return trim($result, '.');
 }
@@ -66,23 +74,23 @@ function ipHex($ip){
 }
 
 function ipParser($ip_str){
-	$ip = array(0,0,0,0);
 	$ip_str_array = preg_split('/[.]/', $ip_str);
-	$count_array=count($ip_str_array);
 	if ($ip_str_array[0] == decbin(FIRST_IP_BYTE)) {
-	   for ($i = 1; $i < 4 ; $i++) { 
-			$ip[$i] = bindec($ip_str_array[$i]) ;
-		}
+	  	typeConverter(create_function('$ip_str_array','return bindec($ip_byte_str)'));
 	} elseif ($ip_str_array[0] == dechex(FIRST_IP_BYTE)) {
-	   for ($i = 1; $i < 4 ; $i++) { 
-			$ip[$i] = hexdec($ip_str_array[$i]) ;
-		}
+	   typeConverter(create_function('$ip_str_array','return hexdec($ip_byte_str)'));
 	} else {
-		for ($i = 1; $i < 4 ; $i++) { 
-			$ip[$i] = (int)($ip_str_array[$i]) ;
-		}
+		typeConverter(create_function('$ip_str_array','return (int)($ip_byte_str)'));
 	}
 	 return $ip;
+
+	function typeConverter($typeFunction){
+		$ip = array(0,0,0,0);
+		for ($i = 1; $i < 4 ; $i++) { 
+			$ip[$i] = typeFunction($ip_str_array[$i]) ;
+		}
+		return $ip;
+	}
 }
 
 function netAddress($ip, $mask){
@@ -93,14 +101,95 @@ function netAddress($ip, $mask){
 	return $conjunction;
 }
 
-//проверка-тест
-$ip = ipGen();
-$mask = maskGen();
-$str = ipBin($ip);
-$str2 = ipBin($mask);
-$network_address = network_address($ip, $mask);
-echo "<h1>".$str."</h1>";
-echo "<h2>".$str2."</h2>";
-echo "<h3>".ipBin($network_address)."</h3>";
-?>
+function netNodesNum($mask){
+	$bin_mask_str =str_replace('.', '', ipBin($mask));
+	$zero_count = 0;
+	$i = strlen($bin_mask_str) - 1; 
+	while ($bin_mask_str[$i] == '0' && $i > 0) {
+		$zero_count++;	
+		$i--;
+	}
+	return pow(2, $zero_count) - 2;
+}
 
+function broadcastAddress($netAddress, $mask) {
+	$addition = array(0,0,0,0);
+	for ($i=0; $i < 4 ; $i++) { 
+		$addition[$i] = ((~$mask[$i]&255) | $netAddress[$i]);
+	}
+	return $addition;
+}
+
+function adressFirstHost($netAddress){
+	return array($netAddress[0], $netAddress[1], $netAddress[2], $netAddress[3]+1);
+}
+
+function adressLastHost($broadcastAddress){
+	return array($broadcastAddress[0], $broadcastAddress[1], $broadcastAddress[2], $broadcastAddress[3]-1);
+}
+
+//тест-проверка
+
+$tasks=array(
+	array('ip' => array(0,0,0,0), 'mask' => array(0,0,0,0)),
+	array('ip' => array(0,0,0,0), 'mask' => array(0,0,0,0)),
+	array('ip' => array(0,0,0,0), 'mask' => array(0,0,0,0))
+);
+
+for ($x=0; $x < 3; $x++) { 
+	$tasks[$x]['ip']=ipGen();
+	$tasks[$x]['mask']=maskGen();
+}
+
+?>
+<table>
+    <tr>
+        <td>Ip-adress and Mask</td>
+        <?php
+        for ($x=0; $x < 3; $x++) { 
+        	$task = taskStrCon($tasks[$x]);
+        	echo "<td>".$task['ip']."<br>".$task['mask']."</td>";
+        }
+        ?>
+    </tr>
+    <tr>
+        <td>Net-adress</td>
+        <?php
+        for ($x=0; $x < 3; $x++) { 
+        	echo "<td>".ipDec(netAddress($tasks[$x]['ip'],$tasks[$x]['mask']))."</td>";
+        }
+        ?>
+    </tr>
+    <tr>
+        <td>Adress first host</td>
+        <?php
+        for ($x=0; $x < 3; $x++) { 
+        	echo "<td>".ipDec(adressFirstHost(netAddress($tasks[$x]['ip'],$tasks[$x]['mask'])))."</td>";
+        }
+        ?>
+    </tr>
+    <tr>
+        <td>Adress last host</td>
+         <?php
+        for ($x=0; $x < 3; $x++) { 
+        	echo "<td>".ipDec(adressLastHost(broadcastAddress($tasks[$x]['ip'],$tasks[$x]['mask'])))."</td>";
+        }
+        ?>
+    </tr>
+    <tr>
+        <td>Broadcast address</td>
+        <?php
+        for ($x=0; $x < 3; $x++) { 
+        	echo "<td>".ipDec(broadcastAddress($tasks[$x]['ip'],$tasks[$x]['mask']))."</td>";
+        }
+        ?>
+    </tr>
+    <tr>
+        <td>Net Nodes Num</td>
+        <?php
+        for ($x=0; $x < 3; $x++) { 
+        	echo "<td>".netNodesNum($tasks[$x]['mask'])."</td>";
+        }
+        ?>
+    </tr>
+</table>
