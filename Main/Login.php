26440ecb29c91c33ef0c529e980c5df8c9a524cd
AsademLige <49205911+AsademLige@users.noreply.github.com>
2019-10-17 18:18:03 +0300

    Сборка проекта
    Страница авторизации соединена с базой

---
+++ Main/Login.php
@@ -0,0 +1,11 @@
+﻿<?php 
+	$group = $_POST['group'];
+	$code = $_POST['code'];
+
+	$link =  mysqli_connect("localhost","root","","cygli");
+	$result = mysqli_query ($link,"SELECT ST_ID FROM STUDENTS S INNER JOIN GROUPS G ON (S.GR_ID = G.GR_ID) WHERE (S.GR_ID = '$group') AND (S.ADC = '$code')");
+
+	while( $row = mysqli_fetch_assoc($result) ){
+		echo $row["ST_ID"];
+	}
+?>
