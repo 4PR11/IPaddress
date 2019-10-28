a824d7c9b1150b47b2e5b850af6a1365890b497e
AsademLige <49205911+AsademLige@users.noreply.github.com>
2019-10-28 15:43:19 +0300

    Скрипт базы данных

--- cygli.sql
+++ cygli.sql
@@ -1,40 +1,39 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.0.10.6
+-- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Окт 21 2019 г., 13:42
--- Версия сервера: 10.3.13-MariaDB
--- Версия PHP: 7.1.22
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 28, 2019 at 03:42 PM
+-- Server version: 5.5.41-log
+-- PHP Version: 5.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- База данных: `cygli`
+-- Database: `cygli`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `groups`
+-- Table structure for table `groups`
 --
 
-CREATE TABLE `groups` (
-  `GR_ID` int(11) NOT NULL,
-  `NAME` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `GR_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`GR_ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=101 ;
 
 --
--- Дамп данных таблицы `groups`
+-- Dumping data for table `groups`
 --
 
 INSERT INTO `groups` (`GR_ID`, `NAME`) VALUES
@@ -43,68 +42,89 @@ INSERT INTO `groups` (`GR_ID`, `NAME`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `loads`
+-- Table structure for table `LISTS`
 --
 
-CREATE TABLE `loads` (
-  `LD_ID` int(11) NOT NULL,
-  `SB_ID` int(11) DEFAULT NULL,
-  `WO_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `LISTS` (
+  `LI_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ST_ID` int(11) NOT NULL,
+  `WR_ID` int(11) NOT NULL,
+  PRIMARY KEY (`LI_ID`),
+  KEY `WO_ID` (`WR_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `logs`
+-- Table structure for table `logs`
 --
 
-CREATE TABLE `logs` (
-  `LG_ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `logs` (
+  `LG_ID` int(11) NOT NULL AUTO_INCREMENT,
   `NT_ID` int(11) DEFAULT NULL,
   `QUEST` varchar(300) DEFAULT NULL,
-  `ANSWER` varchar(300) DEFAULT NULL
+  `ANSWER` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`LG_ID`),
+  KEY `NT_ID` (`NT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mods`
+--
+
+CREATE TABLE IF NOT EXISTS `mods` (
+  `MD_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`MD_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE IF NOT EXISTS `modules` (
+  `MO_ID` int(11) NOT NULL,
+  `NAME` int(11) DEFAULT NULL,
+  PRIMARY KEY (`MO_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `mods`
+-- Table structure for table `notes`
 --
 
-CREATE TABLE `mods` (
-  `MD_ID` int(11) NOT NULL,
-  `NAME` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `notes`
---
-
-CREATE TABLE `notes` (
-  `NT_ID` int(11) NOT NULL,
-  `ST_ID` int(11) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `notes` (
+  `NT_ID` int(11) NOT NULL AUTO_INCREMENT,
   `WR_ID` int(11) DEFAULT NULL,
   `RATING` int(11) DEFAULT NULL,
-  `MD_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `MD_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`NT_ID`),
+  KEY `MD_ID` (`MD_ID`),
+  KEY `WR_ID` (`WR_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `students`
+-- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
-  `ST_ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `students` (
+  `ST_ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(200) DEFAULT NULL,
   `GR_ID` int(11) DEFAULT NULL,
-  `ADC` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `ADC` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`ST_ID`),
+  KEY `GR_ID` (`GR_ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
--- Дамп данных таблицы `students`
+-- Dumping data for table `students`
 --
 
 INSERT INTO `students` (`ST_ID`, `NAME`, `GR_ID`, `ADC`) VALUES
@@ -113,29 +133,46 @@ INSERT INTO `students` (`ST_ID`, `NAME`, `GR_ID`, `ADC`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `subjects`
+-- Table structure for table `subjects`
 --
 
-CREATE TABLE `subjects` (
-  `SB_ID` int(11) NOT NULL,
-  `NAME` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `SB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` int(11) NOT NULL,
+  PRIMARY KEY (`SB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `workers`
+-- Table structure for table `WEIGHTS`
 --
 
-CREATE TABLE `workers` (
-  `WO_ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `WEIGHTS` (
+  `WE_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TASK_NUM` int(11) DEFAULT NULL,
+  `WEIGHT` double DEFAULT NULL,
+  `WR_ID` int(11) NOT NULL,
+  PRIMARY KEY (`WE_ID`),
+  KEY `WO_ID` (`WR_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workers`
+--
+
+CREATE TABLE IF NOT EXISTS `workers` (
+  `WO_ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(200) DEFAULT NULL,
   `PASSWORD` varchar(100) NOT NULL,
-  `LOGIN` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `LOGIN` varchar(100) NOT NULL,
+  PRIMARY KEY (`WO_ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
 
 --
--- Дамп данных таблицы `workers`
+-- Dumping data for table `workers`
 --
 
 INSERT INTO `workers` (`WO_ID`, `NAME`, `PASSWORD`, `LOGIN`) VALUES
@@ -144,177 +181,64 @@ INSERT INTO `workers` (`WO_ID`, `NAME`, `PASSWORD`, `LOGIN`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `works`
+-- Table structure for table `works`
 --
 
-CREATE TABLE `works` (
-  `WR_ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `works` (
+  `WR_ID` int(11) NOT NULL AUTO_INCREMENT,
   `DATE` varchar(50) DEFAULT NULL,
-  `LD_ID` int(11) DEFAULT NULL,
-  `QUEST` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `SB_ID` int(11) NOT NULL,
+  `WO_ID` int(11) NOT NULL,
+  `MO_ID` int(11) NOT NULL,
+  `THEME` varchar(150) NOT NULL,
+  PRIMARY KEY (`WR_ID`),
+  KEY `SB_ID` (`SB_ID`),
+  KEY `WO_ID` (`WO_ID`),
+  KEY `MO_ID` (`MO_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`GR_ID`);
-
---
--- Индексы таблицы `loads`
---
-ALTER TABLE `loads`
-  ADD PRIMARY KEY (`LD_ID`),
-  ADD KEY `WO_ID` (`WO_ID`),
-  ADD KEY `SB_ID` (`SB_ID`);
-
---
--- Индексы таблицы `logs`
---
-ALTER TABLE `logs`
-  ADD PRIMARY KEY (`LG_ID`),
-  ADD KEY `NT_ID` (`NT_ID`);
-
---
--- Индексы таблицы `mods`
---
-ALTER TABLE `mods`
-  ADD PRIMARY KEY (`MD_ID`);
-
---
--- Индексы таблицы `notes`
---
-ALTER TABLE `notes`
-  ADD PRIMARY KEY (`NT_ID`),
-  ADD KEY `MD_ID` (`MD_ID`),
-  ADD KEY `WR_ID` (`WR_ID`),
-  ADD KEY `ST_ID` (`ST_ID`);
-
---
--- Индексы таблицы `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`ST_ID`),
-  ADD KEY `GR_ID` (`GR_ID`);
-
---
--- Индексы таблицы `subjects`
---
-ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`SB_ID`);
-
---
--- Индексы таблицы `workers`
---
-ALTER TABLE `workers`
-  ADD PRIMARY KEY (`WO_ID`);
-
---
--- Индексы таблицы `works`
---
-ALTER TABLE `works`
-  ADD PRIMARY KEY (`WR_ID`),
-  ADD KEY `LD_ID` (`LD_ID`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT для таблицы `groups`
+-- Constraints for table `LISTS`
 --
-ALTER TABLE `groups`
-  MODIFY `GR_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
+ALTER TABLE `LISTS`
+  ADD CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`WR_ID`) REFERENCES `works` (`WR_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- AUTO_INCREMENT для таблицы `loads`
---
-ALTER TABLE `loads`
-  MODIFY `LD_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `logs`
---
-ALTER TABLE `logs`
-  MODIFY `LG_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `mods`
---
-ALTER TABLE `mods`
-  MODIFY `MD_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `notes`
---
-ALTER TABLE `notes`
-  MODIFY `NT_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `students`
---
-ALTER TABLE `students`
-  MODIFY `ST_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT для таблицы `subjects`
---
-ALTER TABLE `subjects`
-  MODIFY `SB_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `workers`
---
-ALTER TABLE `workers`
-  MODIFY `WO_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- AUTO_INCREMENT для таблицы `works`
---
-ALTER TABLE `works`
-  MODIFY `WR_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `loads`
---
-ALTER TABLE `loads`
-  ADD CONSTRAINT `loads_ibfk_1` FOREIGN KEY (`WO_ID`) REFERENCES `workers` (`WO_ID`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `loads_ibfk_2` FOREIGN KEY (`SB_ID`) REFERENCES `subjects` (`SB_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
--- Ограничения внешнего ключа таблицы `logs`
+-- Constraints for table `logs`
 --
 ALTER TABLE `logs`
   ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`NT_ID`) REFERENCES `notes` (`NT_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Ограничения внешнего ключа таблицы `notes`
+-- Constraints for table `notes`
 --
 ALTER TABLE `notes`
   ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`MD_ID`) REFERENCES `mods` (`MD_ID`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`WR_ID`) REFERENCES `works` (`WR_ID`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `notes_ibfk_3` FOREIGN KEY (`ST_ID`) REFERENCES `students` (`ST_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`WR_ID`) REFERENCES `works` (`WR_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Ограничения внешнего ключа таблицы `students`
+-- Constraints for table `students`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`GR_ID`) REFERENCES `groups` (`GR_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Ограничения внешнего ключа таблицы `works`
+-- Constraints for table `WEIGHTS`
+--
+ALTER TABLE `WEIGHTS`
+  ADD CONSTRAINT `weights_ibfk_1` FOREIGN KEY (`WR_ID`) REFERENCES `works` (`WR_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `works`
 --
 ALTER TABLE `works`
-  ADD CONSTRAINT `works_ibfk_1` FOREIGN KEY (`LD_ID`) REFERENCES `loads` (`LD_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
-COMMIT;
+  ADD CONSTRAINT `works_ibfk_3` FOREIGN KEY (`MO_ID`) REFERENCES `modules` (`MO_ID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `works_ibfk_1` FOREIGN KEY (`SB_ID`) REFERENCES `subjects` (`SB_ID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `works_ibfk_2` FOREIGN KEY (`WO_ID`) REFERENCES `workers` (`WO_ID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
